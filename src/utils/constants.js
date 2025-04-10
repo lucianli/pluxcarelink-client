ba4d6aca93b4a34d0304e2760f0df3833dd32344
lucianli <lucianli02@gmail.com>
2025-04-09 21:15:39 -0700

    updated category name

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const categoryColors = {
     'Primary Care': '#94D1FF',
     'Dental Care': '#9BFFED',
     'Vision Care': '#A5E5A5',
-    'Food Resources': '#C7A8FF',
+    'Food Services': '#C7A8FF',
     'Hygiene Resources': '#FF947C',
     'Testing Centers': '#FBEE79',
     'Insurance Navigation': '#FF94C2',
